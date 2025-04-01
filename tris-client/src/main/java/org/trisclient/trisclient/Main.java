ef11af59aa427fb1133917cb6b84862b60773c71
angelo paolella <angpao29@gmail.com>
2025-04-01 16:56:41 +0200

    Modifiche

--- tris-client/src/main/java/org/trisclient/trisclient/Main.java
+++ tris-client/src/main/java/org/trisclient/trisclient/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("GameView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/org/trisclient/trisclient/game-view.fxml"));
         primaryStage.setTitle("Tris Client");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
